b66cbc5ff7eb0af9e520a0a39f9f926238986f55
MichaelBujard <mbujard@udallas.edu>
2021-04-19 15:53:33 +0000

    hist

---
+++ mbhistory.cpp
@@ -0,0 +1,11 @@
+/*
+READ:
+https://en.wikipedia.org/wiki/History_(command)
+
+https://www.digitalocean.com/community/tutorials/how-to-use-bash-history-commands-and-expansions-on-a-linux-vps
+
+https://www.gnu.org/software/bash/manual/html_node/Bash-History-Builtins.html
+
+manpages
+
+*/
