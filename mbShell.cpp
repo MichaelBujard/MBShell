0f582ec75073f541774dbf4b71973ae4e3a5e3d3
MichaelBujard <mbujard@udallas.edu>
2021-04-30 14:55:57 +0000

    added updated code of shell and executable

---
+++ mbShell.cpp
@@ -0,0 +1,55 @@
+#include "mbShell.h"
+
+#include <string.h>
+#include <string>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    mbShell ms;
+    ms.execute();
+}
+
+void mbShell::execute(){
+    while (true){
+        cout << greeting;
+        cin >> command;
+
+        parse_and_execute(command);
+    }
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+* WaitFor(pid)                                                  *
+* Waits for foreground processes...based on Rochkind's waitfor  *
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void mbShell::WaitFor(int pid){
+  int gotpid, status;
+
+  while (true){
+      gotpid = waitpid(pid, &status, 0);
+      if (gotpid == pid) break;
+  }
+  cout << "Process [" << gotpid << "] Finished..." << endl;
+}
+
+void mbShell::parse_and_execute(string c){
+    int id = fork();
+    if (id == 0){ // we are in the child process
+        char* argv[10];
+        argv[1] = NULL;
+        argv[0] = (char*)c.c_str();
+        execvp(argv[0], argv);
+    }
+    else {
+        // Here we are in the parent
+        WaitFor(id);
+    }
+
+    if (c.compare("exit") == 0){
+        exit(0);
+    } else if (c.compare("ls") == 0){
+        cout << "execute ls." << endl;
+    }
+    cout << "That sounds like a good idea." << endl;
+}
